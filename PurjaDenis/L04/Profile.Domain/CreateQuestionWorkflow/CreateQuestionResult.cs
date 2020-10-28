51c227d91292b5f1ccb26c1a979acc050d5a587a
denix52 <71974921+denix52@users.noreply.github.com>
2020-10-28 20:56:05 +0200

    L04

---
+++ PurjaDenis/L04/Profile.Domain/CreateQuestionWorkflow/CreateQuestionResult.cs
@@ -0,0 +1,47 @@
+﻿using CSharp.Choices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Profile.Domain.CreateQuestionWorkflow
+{
+    [AsChoice]
+    public static partial class CreateQuestionResult
+    {
+        public interface ICreateQuestionResult
+        { 
+        
+        }
+        public class QuestionCreated : ICreateQuestionResult
+        {
+            public Guid QuestionId { get; private set; }
+            public string Content { get; private set; }
+
+            public QuestionCreated(Guid questionid, string content)
+            {
+                QuestionId = questionid;
+                Content = content;
+            }
+        }
+        public class QuestionNotCreated:ICreateQuestionResult
+        {
+            public string Reason { get; set; }
+            public QuestionNotCreated(string reason)
+            {
+                Reason = reason;
+            }
+        }
+        public class QuestionValidationFailed:ICreateQuestionResult
+        {
+            public IEnumerable<string> ValidationErrors { get; private set; }
+            public QuestionValidationFailed(IEnumerable<string>errors)
+            {
+
+                ValidationErrors = errors.AsEnumerable();
+            }
+        }
+    }
+
+}
+
---
+++ PurjaDenis/L04/Test.App/ProgramQuestion.cs
@@ -0,0 +1,68 @@
+﻿using Profile.Domain.CreateQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using static Profile.Domain.CreateQuestionWorkflow.CreateQuestionResult;
+
+namespace Test.App
+{
+    class ProgramQuestion
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new CreateQuestionCmd("Titlu", "Continut", "Taguri");
+            var result = CreateQuestion(cmd);
+
+            result.Match(
+                ProcessQuestionCreated, 
+                ProcessQuestionNotCreated, 
+                ProcessInvalidQuestion
+                );
+            Console.ReadLine();
+        }
+
+
+        private static ICreateQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed:");
+            foreach(var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+
+            }
+            return validationErrors;
+        }
+        private static ICreateQuestionResult ProcessQuestionNotCreated(QuestionNotCreated questionNotCreated)
+        {
+            Console.WriteLine($"Question not created:{questionNotCreated.Reason}");
+            return questionNotCreated;
+        }
+        private static ICreateQuestionResult ProcessQuestionCreated(QuestionCreated question)
+        {
+            Console.WriteLine($"Question {question.QuestionId}");
+            return question;
+        }
+
+
+        public static ICreateQuestionResult CreateQuestion(CreateQuestionCmd createQuestion)
+        {
+            if(string.IsNullOrWhiteSpace(createQuestion.Body))
+            {
+                var errors = new List<string>() { "Invalid body" };
+                return new QuestionValidationFailed(errors);
+            }
+            if(string.IsNullOrEmpty(createQuestion.Title))
+            {
+                return new QuestionNotCreated("Enter a valid title");
+            }
+            if(new Random().Next(10)>1)
+            {
+                return new QuestionNotCreated("Question could not be verified");
+            }
+            var questionId = Guid.NewGuid();
+            var result = new QuestionCreated(questionId, createQuestion.Body);
+
+            return result;
+
+        }
+    }
+}
